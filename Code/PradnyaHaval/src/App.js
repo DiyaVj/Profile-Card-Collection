73725790f0d7404bb79283e91f9a88403313feb5
pradnyahaval <56785606+pradnyahaval@users.noreply.github.com>
2022-10-27 16:36:56 +0530

    Add files via upload

---
+++ Code/PradnyaHaval/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProfileCard from './Components/ProfileCard';
+
+function App() {
+  return (
+    <div className="App">
+        <ProfileCard />     
+    </div>
+  );
+}
+
+export default App;
